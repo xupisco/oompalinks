7be7ac91726098d7596091b9b35188eabe646e50
xupisco <xupisco@gmail.com>
2010-03-29 11:06:53 -0300

    New config way

--- settings.py
+++ settings.py
@@ -1,57 +1,38 @@
 # -*- coding: utf-8 -*
 
-import socket, settings
-from app_config import *
+import os, sys, socket, settings
 
-APP_ENV = "development"
+try:
+    if socket.gethostname() == "tomada":
+        from settings_server import *
+    else: 
+        from settings_local import *
+except ImportError:
+    pass
 
-if socket.gethostname() == "tomada":
-    APP_ENV = "production"
-        
 ADMINS = (
     # Ordem alfabética... pra ninguém ficar triste!
     ('Leandro Voltolino', 'xupisco@gmail.com'),
 )
 
 PROJECT_NAME = "oompaLinks"
-GRAPPELLI_ADMIN_TITLE = getattr(settings, "GRAPPELLI_ADMIN_TITLE", PROJECT_NAME)
+USE_GRAPPELLI = True
+ROOT_PATH = os.path.dirname(__file__)
+
 INTERNAL_IPS = ('127.0.0.1', )
-
-
-TEMPLATE_DEBUG = APP_CONFIG[APP_ENV]['debug']
-
-DATABASE_ENGINE =   APP_CONFIG[APP_ENV]['database']['engine']   # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME =     APP_CONFIG[APP_ENV]['database']['name']     # Or path to database file if using sqlite3.
-DATABASE_USER =     APP_CONFIG[APP_ENV]['database']['user']     # Not used with sqlite3.
-DATABASE_PASSWORD = APP_CONFIG[APP_ENV]['database']['password'] # Not used with sqlite3.
-DATABASE_HOST =     APP_CONFIG[APP_ENV]['database']['host']     # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT =     APP_CONFIG[APP_ENV]['database']['port']     # Set to empty string for default. Not used with sqlite3.
-
-CURRENT_SERVER = APP_CONFIG[APP_ENV]['server']['host']
-
-MEDIA_ROOT = os.path.join(ROOT_PATH, 'static')
-MEDIA_URL = 'http://' + CURRENT_SERVER + '/static'
-ADMIN_MEDIA_PREFIX = 'http://' + CURRENT_SERVER + '/media/admin/'
-
-ROOT_URLCONF = 'urls'
 
 #AUTH_PROFILE_MODULE = 'accounts.userprofile'
 
 MANAGERS = ADMINS
 SITE_ID = 1
 
-ugettext = lambda s: s
-
-LANGUAGES = (
-    ('pt_br', ugettext(u'Português do Brasil')),
-    ('en_us', ugettext(u'English')),
-)
+ROOT_URLCONF = 'urls'
+MEDIA_ROOT = os.path.join(ROOT_PATH, 'static')
+MEDIA_URL = 'http://' + CURRENT_SERVER + '/static'
 
 TIME_ZONE = 'America/Sao_Paulo'
 LANGUAGE_CODE = 'en_us' # Eu prefiro o admin em inglês, se quiser pode trocar!
 USE_I18N = True
-
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = 'x$8ok@bp1f00!&q=^&!t_zpv&52_l39rg$#67ptqd^=cuuz#r3'
 
 TEMPLATE_LOADERS = (
@@ -62,25 +43,45 @@ TEMPLATE_LOADERS = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.auth",
     "django.core.context_processors.request",
-    "grappelli.context_processors.admin_url",
+    "django.core.context_processors.debug",
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
-TEMPLATE_DIRS = (
-    os.path.join(ROOT_PATH, "templates"),
-    os.path.join(ROOT_PATH, "templates/admin"),
+ugettext = lambda s: s
+
+LANGUAGES = (
+    ('pt_br', ugettext(u'Português do Brasil')),
+    ('en_us', ugettext(u'English')),
 )
 
+try:
+    import grappelli
+except ImportError:
+    try:
+        import apps.grappelli as grappelli
+        sys.path.append(os.path.join(ROOT_PATH, "apps"))
+    except ImportError:
+        USE_GRAPPELLI = False
+
+if USE_GRAPPELLI:
+    GRAPPELLI_ADMIN_TITLE = getattr(settings, "GRAPPELLI_ADMIN_TITLE", PROJECT_NAME)
+    ADMIN_MEDIA_PREFIX = 'http://' + CURRENT_SERVER + '/media/admin/'
+    TEMPLATE_DIRS = (
+        os.path.join(ROOT_PATH, "templates"),
+        os.path.join(ROOT_PATH, "templates/admin"),
+    )
+    
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.admin',
+    'apps.links',
     'grappelli',
+    #'tagging',
 )
--- settings_local.py
+++ settings_local.py
@@ -1,65 +1,14 @@
 # -*- coding: utf-8 -*
 
-import os, sys
-ROOT_PATH = os.path.dirname(__file__)
-sys.path.append(os.path.join(ROOT_PATH, "complink"))
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/Users/leandro/Sites/complink/db/data.db'   # Or path to database file if using sqlite3.
+DATABASE_NAME = '/Users/leandro/Sites/oompalinks/db/data.db'   # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 CURRENT_SERVER = "127.0.0.1:8000"
-CURRENT_SERVER = "192.168.1.201:8000"
-USE_GRAPPELLI = True
-
-MEDIA_ROOT = os.path.join(ROOT_PATH, 'static')
-MEDIA_URL = 'http://' + CURRENT_SERVER + '/static'
-
-if USE_GRAPPELLI:
-    ADMIN_MEDIA_PREFIX = 'http://' + CURRENT_SERVER + '/media/admin/'
-
-ROOT_URLCONF = 'urls'
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
-    "django.core.context_processors.request",
-    "django.core.context_processors.debug",
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
-
-if USE_GRAPPELLI:
-    TEMPLATE_DIRS = (
-        os.path.join(ROOT_PATH, "templates"),
-        os.path.join(ROOT_PATH, "templates/admin"),
-    )
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.admin',
-    'apps.links',
-    #'tagging',
-    'grappelli',
-)
-
-if os.path.exixts('apps/grappelli'):
-    INSTALLED_APPS.append('apps/grappelli')
-else 
-    INSTALLED_APPS.append('grappelli')
+#CURRENT_SERVER = "192.168.1.201:8000"
